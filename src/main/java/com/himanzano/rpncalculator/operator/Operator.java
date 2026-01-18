6fa43128b5c70c02b9647180c7bcae0e1bf22dd9
Marlon Couto <ozaircouto@outlook.com>
2026-01-18 20:06:26 -0300

    feat(operator): add mapping from character to Operator enum

--- src/main/java/com/himanzano/rpncalculator/operator/Operator.java
+++ src/main/java/com/himanzano/rpncalculator/operator/Operator.java
@@ -2,4 +2,15 @@ package com.himanzano.rpncalculator.operator;
 
 public enum Operator {
     ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION, NEGATIVE;
+
+    public static Operator mapOperator(char c) {
+        return switch (c) {
+            case '+' -> ADDITION;
+            case '*' -> MULTIPLICATION;
+            case '/' -> DIVISION;
+            case '-' -> SUBTRACTION;
+            case '~' -> NEGATIVE;
+            default -> throw new IllegalArgumentException("Unknown operator: " + c);
+        };
+    }
 }
