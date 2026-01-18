3ae08d8474a6783546f501db218007b99e7c5452
Marlon Couto <ozaircouto@outlook.com>
2026-01-18 20:27:59 -0300

    feat(tokenizer): implement tokenize method for parsing expressions into tokens

--- src/main/java/com/himanzano/rpncalculator/tokenizer/Tokenizer.java
+++ src/main/java/com/himanzano/rpncalculator/tokenizer/Tokenizer.java
@@ -1,4 +1,70 @@
 package com.himanzano.rpncalculator.tokenizer;
 
+import com.himanzano.rpncalculator.operator.Operator;
+import com.himanzano.rpncalculator.token.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tokenizer {
+    public List<Token> tokenize(String input) {
+        List<Token> tokens = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+
+        char[] chars = input.toCharArray();
+        for (char c : chars) {
+            if (!Character.isDigit(c) && c != '.' && !sb.isEmpty()) {
+                String str = sb.toString();
+                double number = Double.parseDouble(str);
+                NumberToken token = new NumberToken(number);
+                tokens.add(token);
+                sb.setLength(0);
+            }
+
+            if (c == '(') {
+                OpeningParenthesisToken token = new OpeningParenthesisToken();
+                tokens.add(token);
+                continue;
+            }
+
+            if (c == ')') {
+                ClosingParenthesisToken token = new ClosingParenthesisToken();
+                tokens.add(token);
+                continue;
+            }
+
+            if (c == '-') {
+                OperatorToken token = new OperatorToken(Operator.SUBTRACTION);
+                tokens.add(token);
+                continue;
+            }
+
+            if (c == '+' || c == '*' || c == '/') {
+                Operator op = Operator.mapOperator(c);
+                OperatorToken token = new OperatorToken(op);
+                tokens.add(token);
+                continue;
+            }
+
+            if (Character.isDigit(c) || c == '.') {
+                sb.append(c);
+                continue;
+            }
+
+            if (Character.isWhitespace(c)) {
+                continue;
+            }
+
+            throw new IllegalArgumentException("Illegal character " + c);
+        }
+
+        if (!sb.isEmpty()) {
+            String str = sb.toString();
+            double number = Double.parseDouble(str);
+            NumberToken token = new NumberToken(number);
+            tokens.add(token);
+        }
+
+        return tokens;
+    }
 }
